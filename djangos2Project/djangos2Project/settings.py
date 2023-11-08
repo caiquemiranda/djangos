17515abdbf605d89d6e2fcb1cfce51201891cb3d
caiquemiranda <caiquemirandas@gmail.com>
2023-11-08 05:23:11 -0300

    adc_photo_bD

--- djangos2Project/djangos2Project/settings.py
+++ djangos2Project/djangos2Project/settings.py
@@ -117,9 +117,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
 STATICFILES_DIRS = [
     'statics',
 ]
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
